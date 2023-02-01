313cdd626b0bf77d705c754204b5b04408c417dd
Philipp Nowak <git@lit.plus>
2023-02-01 01:37:02 +0100

    Set up logging & error handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
+use anyhow::Context;
 use clap::{Parser, Subcommand};
+use clap_verbosity_flag::{InfoLevel, Verbosity};
+use flexi_logger::{colored_default_format, detailed_format, Logger, LoggerHandle, WriteMode};
+use log::{debug, info, Level};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
 struct Cli {
-    /// Enable debug mode, repeat to ✨ intensify ✨
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    verbose: u8,
+    #[clap(flatten)]
+    verbose: Verbosity<InfoLevel>,
+
+    /// Use a flexi_logger configuration file
+    #[arg(long="log-spec")]
+    use_log_spec: bool,
+
+    /// Path to log spec
+    #[arg(long, value_name="TOML FILE", default_value="logspec.toml")]
+    log_spec_file: std::path::PathBuf,
 
     #[command(subcommand)]
     command: Commands,
@@ -21,22 +32,44 @@ enum Commands {
     }
 }
 
-fn main() {
-    let cli = Cli::parse();
+fn configure_logging(cli: &Cli) -> anyhow::Result<LoggerHandle> {
+    // log_level() returns None iff verbosity < 0, i.e. being most quiet seems reasonable
+    let cli_level = cli.verbose.log_level()
+        .unwrap_or(Level::Error);
 
-    match cli.verbose {
-        0 => println!("Zero debug for you"),
-        1 => println!("Lowkey debugging"),
-        2 => println!("No bugs left"),
-        _ => println!("Bug count negative, police have been informed"),
-    }
+    let log_builder = Logger::try_with_env_or_str(cli_level.to_string())
+        .context("Failed to parse logger spec from env RUST_LOG or cli level")?
+        // TODO: switch to async in release builds https://stackoverflow.com/a/39205417 ?
+        .write_mode(WriteMode::BufferAndFlush)
+        .format_for_stdout(colored_default_format)
+        .format_for_files(detailed_format);
+
+    return match (&cli.use_log_spec, &cli.log_spec_file) {
+        (true, specfile_path) => log_builder
+            .start_with_specfile(specfile_path)
+            .with_context(|| format!("Failed to start logger with specfile {:?}", *specfile_path)),
+        (false, _) => log_builder
+            .start().context("Failed to start logger handle w/o specfile")
+    };
+}
+
+fn main() -> anyhow::Result<()> {
+    // need explicit type annotation for IntelliJ
+    let cli: Cli = Cli::parse();
+    let logger_handle = configure_logging(&cli)
+        .context("Unable to configure logging")?;
 
     match cli.command {
         Commands::Greet { name } => {
             println!("Henlo {}", name.as_str());
-            if cli.verbose > 1 {
-                println!("HELLO {}!!", name.as_str());
-            }
+            debug!("HELLO {}!!", name.as_str());
+            info!("Greeting performed successfully.")
         }
     }
+
+    // Important with non-direct write mode
+    // Handle needs to be kept alive until end of program
+    logger_handle.flush();
+
+    Ok(())
 }
