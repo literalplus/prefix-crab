886a45352299681a9a10904aca6d7a6355dca8a1
Philipp Nowak <git@lit.plus>
2023-10-28 18:26:05 +0200

    Fix rmq wrapper

--- deploy/run-with-rmq-pw.sh
+++ deploy/run-with-rmq-pw.sh
@@ -4,9 +4,6 @@ SECRET_NAME=prefix-crab-rmq-password
 
 SECRET_VALUE=$(podman run --secret "$SECRET_NAME" --rm docker.io/alpine cat "/run/secrets/$SECRET_NAME" || (echo "Failed to read credential!" && exit 17))
 
-echo "oops we forgot to remove this: $SECRET_VALUE"
-export RMQ_PW="$SECRET_VALUE"
+echo "Providing secret $SECRET_NAME in environment as \$RMQ_PW (for ${@})".
 
-echo "Provided secret $SECRET_NAME to environment as \$RMQ_PW".
-
-"${@}"
+RMQ_PW="$SECRET_VALUE" "${@}"
