5359d249d0642835fda437505a95b0a93ec3831a
Philipp Nowak <git@lit.plus>
2023-10-29 13:13:19 +0100

    Fix push-bins.sh

--- deploy/push-bins.sh
+++ deploy/push-bins.sh
@@ -7,9 +7,14 @@ pushd .. || exit 17
 make docker-builder
 
 TMP_CONTAINER_ID=$(docker create prefix-crab.local/builder)
+
 echo "Uploading yarrp-buddy..."
-docker cp $TMP_CONTAINER_ID:/usr/src/prefix-crab/target/release/yarrp-buddy - | ssh $TARGET_HOST bash -c "cat > ./prefix-crab/deploy/bin/yarrp-buddy"
+docker cp $TMP_CONTAINER_ID:/usr/src/prefix-crab/target/release/yarrp-buddy - | ssh $TARGET_HOST "tar -Oxf - >./prefix-crab/deploy/bin/yarrp-buddy"
+ssh $TARGET_HOST chmod 700 ./prefix-crab/deploy/bin/yarrp-buddy
+
 echo "Uploading zmap-buddy..."
-docker cp $TMP_CONTAINER_ID:/usr/src/prefix-crab/target/release/zmap-buddy - | ssh $TARGET_HOST bash -c "cat > ./prefix-crab/deploy/bin/zmap-buddy"
+docker cp $TMP_CONTAINER_ID:/usr/src/prefix-crab/target/release/zmap-buddy - | ssh $TARGET_HOST "tar -Oxf - >./prefix-crab/deploy/bin/zmap-buddy"
+ssh $TARGET_HOST chmod 700 ./prefix-crab/deploy/bin/zmap-buddy
+
 docker rm -v $TMP_CONTAINER_ID
 echo "Done."
