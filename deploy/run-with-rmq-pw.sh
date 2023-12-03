01a2e8d95a4b162905d3dd589f539b2f429ee787
Philipp Nowak <git@lit.plus>
2023-12-03 14:57:27 +0100

    Maybe don't log RabbitMQ password in journal

--- deploy/run-with-rmq-pw.sh
+++ deploy/run-with-rmq-pw.sh
@@ -2,7 +2,7 @@
 
 SECRET_NAME=prefix-crab-rmq-password
 
-SECRET_VALUE=$(podman run --secret "$SECRET_NAME" --rm docker.io/alpine cat "/run/secrets/$SECRET_NAME" || (echo "Failed to read credential!" && exit 17))
+SECRET_VALUE=$(podman run --secret "$SECRET_NAME" --log-driver=none --rm docker.io/alpine cat "/run/secrets/$SECRET_NAME" || (echo "Failed to read credential!" && exit 17))
 
 echo "Providing secret $SECRET_NAME in environment as \$RMQ_PW (for ${@})".
 
