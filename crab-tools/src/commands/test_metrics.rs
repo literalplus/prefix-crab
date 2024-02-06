6819371b4e718181b94e1d38b95b704a3d7b4386
Philipp Nowak <git@lit.plus>
2024-02-07 00:34:59 +0100

    Add tracing example to crab-tools

--- crab-tools/src/commands/test_metrics.rs
+++ crab-tools/src/commands/test_metrics.rs
@@ -7,9 +7,12 @@ use lazy_static::lazy_static;
 use opentelemetry::{
     global,
     metrics::{Counter, Meter},
+    KeyValue,
 };
-use opentelemetry_otlp::WithExportConfig;
-use tokio::time;
+use opentelemetry_otlp::{HttpExporterBuilder, WithExportConfig};
+use opentelemetry_sdk::{runtime::Tokio, trace, Resource};
+use tracing::instrument;
+use tracing_subscriber::{filter::filter_fn, layer::SubscriberExt, Layer, Registry};
 
 lazy_static! {
     static ref METER: Meter = global::meter("prefix-crab.local/crab-tools");
@@ -33,30 +36,64 @@ pub fn handle(params: Params) -> Result<()> {
 
     executor::block_on(handle)??;
 
+    global::shutdown_tracer_provider(); // must be called outside of Tokio, otherwise blocks forever
+
     Ok(())
 }
 
 async fn run(params: Params) -> Result<()> {
     println!("Sending metrics to {}", params.endpoint);
-    let exporter = opentelemetry_otlp::new_exporter()
-        .http()
-        .with_protocol(opentelemetry_otlp::Protocol::HttpBinary)
-        .with_endpoint(params.endpoint)
-        .with_headers(HashMap::from([(
-            "Authorization".to_owned(),
-            params.headers,
-        )]));
-
-    let meter_provider = opentelemetry_otlp::new_pipeline()
-        .metrics(opentelemetry_sdk::runtime::Tokio)
+    opentelemetry_otlp::new_pipeline()
+        .metrics(Tokio)
         .with_period(Duration::from_secs(5))
         .with_timeout(Duration::from_secs(3))
-        .with_exporter(exporter)
+        .with_exporter(make_exporter(&params))
         .build()?;
-    COUNTER.add(2, &[]);
-    time::sleep(Duration::from_secs(30)).await;
 
-    println!("Shutdown result: {:?}", meter_provider.shutdown()); // https://github.com/open-telemetry/opentelemetry-rust/pull/1375
+    let tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(make_exporter(&params))
+        .with_trace_config(
+            trace::config().with_resource(Resource::new(vec![KeyValue::new(
+                "service.name",
+                "crab-tools",
+            )])),
+        )
+        .install_batch(Tokio)?;
+
+    let telemetry = tracing_opentelemetry::layer()
+        .with_tracer(tracer)
+        .with_filter(filter_fn(|metadata| {
+            metadata.module_path() != Some("isahc::handler")
+        }));
+    let subscriber = Registry::default().with(telemetry);
+
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    my_test_fun("hello its me").await;
+
+    COUNTER.add(2, &[]);
 
     Ok(())
+}
+
+fn make_exporter(params: &Params) -> HttpExporterBuilder {
+    opentelemetry_otlp::new_exporter()
+        .http()
+        .with_protocol(opentelemetry_otlp::Protocol::HttpBinary)
+        .with_endpoint(&params.endpoint)
+        .with_headers(HashMap::from([(
+            "Authorization".to_owned(),
+            params.headers.to_owned(),
+        )]))
+}
+
+#[instrument]
+async fn my_test_fun(param: &str) {
+    sub_fun(param);
+}
+
+#[instrument]
+fn sub_fun(param: &str) {
+    tracing::warn!("oh no! a {}", param)
 }
